723f44074add356c4e9339f4528724fab1068775
Wilbax <janithravisankax@gmail.com>
2024-06-27 20:40:26 +0530

    added dockefile

--- Charts(8002)/main.py
+++ Charts(8002)/main.py
@@ -19,4 +19,4 @@ app.include_router(user)
 
 if __name__ == "__main__":
     # Specify the host and port here
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8002)
